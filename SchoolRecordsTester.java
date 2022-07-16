207d2b26181df95c3aa3e7e7499d423dc5c8c182
Jayakumar E <jayakumar.e@prodapt.com>
2022-07-16 17:33:04 +0530

    Tester

---
+++ SchoolRecordsTester.java
@@ -0,0 +1,15 @@
+package prodapt;
+
+public class SchoolRecordsTester {
+	 public static void main(String[] args) {
+	
+	SchoolRecords ob1 = new SchoolRecords();
+	ob1.dispRecords();
+	
+	ob1.addRecords(1234 ,"Orchids The International School"," 4th St, near Cognizant TCO, Sri Sowdeswari Nagar, Thoraipakkam, Tamil Nadu 600097");
+	ob1.addRecords(1235 ,"Maharishi Vidya Mandir Senior Secondary School"," Inside Maharishi Gardens, Dr Gurusamy Rd, Chetpet, Chennai, Tamil Nadu");
+
+	ob1.addRecords(1236 ,"National Public School","228, Avvai Shanmugam Salai, Ganapathy Colony, Gopalapuram, Chennai, Tamil Nadu 600086");
+
+}
+}
