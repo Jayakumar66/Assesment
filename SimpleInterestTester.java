3cb8ecf07e69fc1cee65e5b075863986f48287a4
Jayakumar E <jayakumar.e@prodapt.com>
2022-07-16 17:36:02 +0530

    SimpleTester

---
+++ SimpleInterestTester.java
@@ -0,0 +1,12 @@
+package prodapt;
+
+public class SimpleInterestTester {
+	public static void main(String args[]){
+		SimpleInterest.principle=1500;
+		SimpleInterest.rate=12;
+		SimpleInterest.time=3;
+		SimpleInterest.simple_Interest();
+		
+	}
+
+}
