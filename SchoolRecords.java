19a55618c76ac97b13c18aa8e6430031aeb0b783
Jayakumar E <jayakumar.e@prodapt.com>
2022-07-16 17:29:59 +0530

    Source

---
+++ SchoolRecords.java
@@ -0,0 +1,16 @@
+package prodapt;
+
+public class SchoolRecords {
+	int schoolId;
+	String schoolName;
+	String schoolAddress;
+	void addRecords(int schoolId,String schoolName,String schoolAddress) {
+		System.out.println(schoolId+" "+schoolName+" "+schoolAddress);
+	
+	}
+	void dispRecords() {
+		System.out.println("School Name Address Will be Showing");
+	}
+	
+
+}
